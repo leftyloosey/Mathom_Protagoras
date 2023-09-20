86023eae72bad3540d91ec78d61b83a0159d858a
davidhardin <thisgoestodavid@gmail.com>
2023-09-19 18:46:23 -0600

    searchusers

--- github-finder/src/components/users/UserSearch.jsx
+++ github-finder/src/components/users/UserSearch.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
 
 const UserSearch = () => {
+  const [text, setText] = useState('')
+
+  const { users } = useContext(GithubContext)
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (text === '') {
+      //   setAlert('Please enter something', 'error')
+      alert('please enter something')
+    } else {
+      //   dispatch({ type: 'SET_LOADING' })
+      //   const users = await searchUsers(text)
+      //   dispatch({ type: 'GET_USERS', payload: users })
+
+      setText('')
+    }
+  }
+
   return (
     <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='form-control'>
             <div className='relative'>
               <input
                 type='text'
                 className='w-full pr-40 bg-gray-200 input input-lg text-black'
+                placeholder='Search'
+                value={text}
+                onChange={handleChange}
               />
+              <button className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'>
+                Go
+              </button>
             </div>
           </div>
         </form>
       </div>
-      <div>
-        <button className='btn btn-ghost btn-lg'>Clear</button>
-      </div>
+      {users.length > 0 && (
+        <div>
+          <button className='btn btn-ghost btn-lg'>Clear</button>
+        </div>
+      )}
     </div>
   )
 }
-// now what
+
 export default UserSearch
