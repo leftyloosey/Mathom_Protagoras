dfb8d854a55a2313bbfcf3c77cb7f26cf8578ecd
davidhardin <thisgoestodavid@gmail.com>
2023-09-24 12:56:20 -0600

    websiteurl

--- github-finder/src/components/pages/User.jsx
+++ github-finder/src/components/pages/User.jsx
@@ -31,7 +31,8 @@ const User = () => {
     hireable,
   } = user
 
-  const websiteUrl = 'https://hardinportfolio.herokuapp.com/'
+  // const websiteUrl = 'https://hardinportfolio.herokuapp.com/'
+  const websiteUrl = blog?.startsWith('http') ? blog : 'https://' + blog
 
   if (loading) {
     return <Spinner />
